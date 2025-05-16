7e52204fae3523c9dab1095b234c225b27518e1b
yuri-brito <yurisb85@gmail.com>
2025-05-16 09:28:20 -0300

    inclusão nome do repositório

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/nome-do-repo/",
+  base: "/mbsouza/",
   plugins: [react()],
 });
